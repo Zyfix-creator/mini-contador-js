29ff43f650b518eb46f85b9a254cbb9a11c6b713
Zyfix <ZyfixT@proton.me>
2026-02-23 15:46:56 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,30 @@
-const number = 100;
-const direção = "c";
+// index.js
+const prompt = require("prompt-sync")({ sigint: true }); // necessário para Node.js
 
-if(direção === "c"){
-  for(let i = 0; i < number; i++){
+const number = Number(prompt("Digite um número para contar: "));
+const direcao = prompt("Quer contar crescente ou decrescente? (c/d): ");
+
+// Contagem
+if(direcao === "c"){
+  for(let i = 1; i <= number; i++){
     console.log(i);
   }
-} else if(direção === "d"){
-  for(let i = number; i > 0; i--){
+} else if(direcao === "d"){
+  for(let i = number; i >= 1; i--){
     console.log(i);
   }
 } else {
   console.log("Opção inválida");
 }
 
+// Classificação
 switch(true){
   case number < 5:
-    console.log("Contagem pequena")
-    break
+    console.log("Contagem pequena");
+    break;
   case number >= 5 && number < 10:
-    console.log("Contagem média")
-    break
-  case number >= 10:
-    console.log("Contagem grande")
-    break
+    console.log("Contagem média");
+    break;
+  default:
+    console.log("Contagem grande");
 }
-
